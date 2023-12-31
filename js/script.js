f49dd3e4fd42401138f9bc414fa5d93bc33a1ae1
diaby mamaduo <diabymamadou3001@gmail.com>
2023-12-31 02:44:47 +0100

    add animation

--- js/script.js
+++ js/script.js
@@ -7,3 +7,60 @@ window.addEventListener("load", () => {
     loader.style.display = "none";
   }, 500);
 });
+
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting){
+      if(entry.target.id == "overlayLateralFollow"){
+        entry.target.classList.add('bounce-in-top');
+        entry.target.classList.remove('hidden');
+      }
+      if(entry.target.id == "nav"){
+        entry.target.classList.add('tracking-in-expand-fwd-top');
+      }
+      if(entry.target.id == "home__container"){
+        entry.target.classList.add('tracking-in-expand-fwd-bottom');
+        setTimeout(() =>{
+          entry.target.children[3].classList.add('pulsate-fwd');
+        },800)
+      }
+      if(entry.target.id == "work__container__contentFirst"){
+        entry.target.classList.add("tracking-in-expand-fwd-top");
+        entry.target.children[1].children[0].classList.add('pulsate-fwd');
+      }
+      if(entry.target.id == "work__container__contentSecond__row__item"){
+        entry.target.classList.add("show");
+      }
+      if(entry.target.id == "aboutMe__container__contentFirst"){
+        entry.target.classList.add("show");
+        entry.target.children[1].children[0].classList.add('pulsate-fwd');
+      }
+      if(entry.target.id == "aboutMe__container__contentSecond"){
+        entry.target.classList.add("show");
+      }
+      if(entry.target.id == "aboutMe__container__contentThird"){
+        entry.target.classList.add("tracking-in-expand-fwd-top");
+      }
+      if(entry.target.id == "aboutMe__container__contentFourth__container__item"){
+        entry.target.classList.add("show");
+      }
+      if(entry.target.id == "aboutMe__container__contentFifth"){
+        entry.target.classList.add("tracking-in-expand-fwd-top");
+        setTimeout(() =>{
+          console.log(entry.target.children[1].children[0]);
+        },800)
+      }
+      if(entry.target.id == "aboutMe__container__contentSixth"){
+        entry.target.classList.add("tracking-in-expand-fwd-top");
+      }
+      
+      
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
